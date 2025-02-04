de04efbe18c8c945890701f7603ae9798f339d09
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-04 13:25:24 +0600

    get the all html element

---
+++ script.js
@@ -0,0 +1,4 @@
+const progress = document.querySelector(".progress");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const iconsWrapper = document.querySelectorAll(".icon-wrapper");
