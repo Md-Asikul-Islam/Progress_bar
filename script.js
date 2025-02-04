5639ce7cd42190eccdb68406e85c576a04f01e29
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-04 13:26:32 +0600

    next buttin adding to progress bar

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const progress = document.querySelector(".progress");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const iconsWrapper = document.querySelectorAll(".icon-wrapper");
+
+let currentSelectedStep = 1;
+
+nextBtn.addEventListener("click", () => {
+  if (currentSelectedStep < iconsWrapper.length) {
+    currentSelectedStep++;
+  }
+
+  handleUpdateStep();
+});
